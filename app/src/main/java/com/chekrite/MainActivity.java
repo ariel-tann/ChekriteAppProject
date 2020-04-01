64de4f70e312aaf02779228e9b35550e6c4e7e08
KaiHua Chuang <stu10064@hotmail.com>
2020-04-01 13:20:20 +1000

    add requesting permission function for camera and audio recording

--- app/src/main/java/com/chekrite/MainActivity.java
+++ app/src/main/java/com/chekrite/MainActivity.java
@@ -4,21 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.chekrite.permission.Permission;
+
+import java.util.List;
 
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Permission mPermission;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("KAI", this.getComponentName()+"");
+        mPermission = new Permission(this);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // Forward results to EasyPermissions
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    protected void onStart() {
+        super.onStart();
+        //check permissions are all allowed
+        mPermission.RequestPermissions();
+        
     }
 }
--- app/src/main/java/com/chekrite/permission/Permission.java
+++ app/src/main/java/com/chekrite/permission/Permission.java
@@ -1,31 +1,34 @@
 package com.chekrite.permission;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.chekrite.R;
 
 import pub.devrel.easypermissions.EasyPermissions;
 
+
 // use this class to ask the user for permission to camera recording etc..
-public class Permission {
-    Context mContext;
-    public Permission(Context context) {
-        mContext = context;
+public class Permission{
+    Activity mActivity;
+    private int mRequestCode = 256;
+    public Permission(Activity activity) {
+        mActivity = activity;
+        // Will request all permissions from the Manifest automatically.
+        RequestPermissions();
     }
-//    @AfterPermissionGranted(RC_CAMERA_AND_LOCATION)
-//    private void methodRequiresTwoPermission() {
-//        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION};
-//        if (EasyPermissions.hasPermissions(this, perms)) {
-//            // Already have permission, do the thing
-//            // ...
-//        } else {
-//            // Do not have permissions, request them now
-//            EasyPermissions.requestPermissions(this, R.string.rationale_camera,
-//                    RC_CAMERA_AND_LOCATION, perms);
-//        }
-//    }
+    public void RequestPermissions() {
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+        if (!EasyPermissions.hasPermissions(mActivity, perms)) {
+            //Request Permissions
+            EasyPermissions.requestPermissions(mActivity,
+                    mActivity.getString(R.string.rationale_camera_audio),
+                    mRequestCode, perms);
+        } else {
+            // Already have permission, do the thing
+            // ...
+        }
+
+    }
 }
