4c28819483b10387083ae23176e27dadfe77b282
hanchuan liu <atm87526@gmail.com>
2020-04-06 00:23:05 +1000

    go to New Check

--- app/src/main/java/com/chekrite/dashBoard/Dashboard.java
+++ app/src/main/java/com/chekrite/dashBoard/Dashboard.java
@@ -8,15 +8,25 @@ package com.chekrite.dashBoard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.chekrite.R;
+import com.chekrite.select_asset_screen.Select_asset_screen;
 
 public class Dashboard extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+
+    }
+    public void startNewcheck(View view){
+        Intent intent = new Intent(this, Select_asset_screen.class);
+        startActivity(intent);
     }
 }
