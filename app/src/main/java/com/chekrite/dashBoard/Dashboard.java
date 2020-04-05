5dcf9f7b1a70b287b5079428569dc9d01b9794ed
hanchuan liu <atm87526@gmail.com>
2020-04-05 23:10:51 +1000

    Dashboard Screen

---
+++ app/src/main/java/com/chekrite/dashBoard/Dashboard.java
@@ -0,0 +1,22 @@
+/*
+ * Date: 2020.4.5
+ * This file is created by Kai.
+ * Summary:
+ */
+
+package com.chekrite.dashBoard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chekrite.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ app/src/main/java/com/chekrite/dashBoard/SyncNow.java
@@ -0,0 +1,10 @@
+/*
+ * Date: 2020.4.5
+ * This file is created by Kai.
+ * Summary:
+ */
+
+package com.chekrite.dashBoard;
+
+public class SyncNow {
+}
