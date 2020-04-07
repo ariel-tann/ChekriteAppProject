987011d1e8a85acf6356538b75ee946dc33bea68
Ariel <arieltan.qut@gmail.com>
2020-04-08 08:49:35 +1000

    login screen frame

---
+++ app/src/main/java/com/chekrite/Login.java
@@ -0,0 +1,20 @@
+/*
+ * Date: 2020.4.8
+ * This file is created by Kai.
+ * Summary:
+ */
+
+package com.chekrite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
