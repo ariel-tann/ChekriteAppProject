0772b7b0c7e2da4ce5343cecb2a6cf9bb89d5fac
KaiHua Chuang <stu10064@hotmail.com>
2020-05-09 13:05:31 +1000

    fix response_timestamp bug

--- app/src/main/java/com/chekrite_group44/Tests/fragment_button.java
+++ app/src/main/java/com/chekrite_group44/Tests/fragment_button.java
@@ -9,12 +9,14 @@ package com.chekrite_group44.Tests;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,9 +26,9 @@ import androidx.fragment.app.Fragment;
 import com.chekrite_group44.Asset_Properties.Inspection_checklist_item;
 import com.chekrite_group44.R;
 
-public class fragment_button extends Fragment {
-    private Button mbtn_1;
-    private Button mbtn_2;
+import java.util.ArrayList;
+
+public class fragment_button extends Fragment implements View.OnClickListener{
     private Inspection_checklist_item mItem;
     TextView mDescription;
     TextView mTitle;
@@ -35,6 +37,8 @@ public class fragment_button extends Fragment {
     int position;
     Long start;
     InspectionListener mlistener;
+    int col = 2; // define number of buttons should be in one row
+
     public fragment_button(Inspection_checklist_item item, int total_items, int position, InspectionListener listener) {
         mItem = item;
         this.total_items = total_items;
@@ -56,52 +60,81 @@ public class fragment_button extends Fragment {
         mProgressBar.setProgress((int)(((double)position+1)/(double)total_items*100));
         // get item start timestamp
         start = System.currentTimeMillis();
-        String btn1_color = mItem.getControl().getButtons().get(0).getColor();
-        String lbl1 = mItem.getControl().getButtons().get(0).getLabel();
-        String btn2_color = mItem.getControl().getButtons().get(1).getColor();
-        String lbl2 = mItem.getControl().getButtons().get(1).getLabel();
-        mbtn_1 = view.findViewById(R.id.inspection_btn1);
-        mbtn_1.setText(lbl1);
-        mbtn_1.setTextSize(20);
-        mbtn_1.setAllCaps(false);
-        mbtn_1.setTextColor(view.getContext().getColor(R.color.white));
-        // set background color and stroke color
-        mbtn_1.setBackground(drawCircle(Color.parseColor(btn1_color),
-                view.getContext().getColor(R.color.dark_gray)));
-        mbtn_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO　call response api and navigate to next fragment
-                Long end = System.currentTimeMillis();
-                mlistener.Completed(Control_TYPE.button,0,0,
-                        start, end);
+        //
+        TableLayout tableLayout = view.findViewById(R.id.insp_t_layout);
+        // create 2 rows
+        ArrayList<TableRow> rows = new ArrayList<>();
+        // num of rows
+        int num_btn = mItem.getControl().getButtons().size();
+        // num of column
+
+        int num_rows = (int)Math.ceil(num_btn/(double)col);
+
+        for (int i = 0; i< num_rows; i++){
+            TableRow row = new TableRow(view.getContext());
+            row.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT));
+            row.setGravity(Gravity.CENTER);
+            rows.add(i, row);
+            tableLayout.addView(row);
+        }
+        // use TableRow LayoutParams to set width and height
+        TableRow.LayoutParams layoutParams =
+                new TableRow.LayoutParams(300, 300);
+        layoutParams.setMargins(50, 50, 50, 50);
+
+        // add 2 button in each row
+        ArrayList<Button> btns = new ArrayList<>();
+        int btn_id = 0;
+        for (int i = 0; i < rows.size(); i++){
+            for (int j = 0; j < col ; j++){
+                // get info from button obj
+                String btn_color = mItem.getControl().getButtons().get(btn_id).getColor();
+                String lbl = mItem.getControl().getButtons().get(btn_id).getLabel();
+
+                Button btn = new Button(view.getContext());
+                btn.setId(btn_id);
+                btn.setText(lbl);
+                btn.setTextSize(20);
+                btn.setAllCaps(false);
+                btn.setLayoutParams(layoutParams);
+                btn.setTextColor(view.getContext().getColor(R.color.white));
+                btn.setBackground(drawCircle(Color.parseColor(btn_color),
+                        view.getContext().getColor(R.color.dark_gray)));
+                if(btn_id + 1 <= num_btn){
+                    btn.setOnClickListener(this);
+                }else{
+                    btn.setVisibility(Button.INVISIBLE);
+                }
+                // add btn in table row
+                rows.get(i).addView(btn);
+                btns.add(btn);
+                btn_id += 1;
             }
-        });
-        mbtn_2 = view.findViewById(R.id.inspection_btn2);
-        mbtn_2.setText(lbl2);
-        mbtn_2.setTextSize(20);
-        mbtn_2.setAllCaps(false);
-        mbtn_2.setTextColor(view.getContext().getColor(R.color.white));
-        // set background color and stroke color
-        mbtn_2.setBackground(drawCircle(Color.parseColor(btn2_color),
-                view.getContext().getColor(R.color.dark_gray)));
-        mbtn_2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO　call response api and navigate to next fragment
-                Long end = System.currentTimeMillis();
-                mlistener.Completed(Control_TYPE.button,1,0,
-                        start, end);
-            }
-        });
+        }
+        //
         return view;
     }
     private static GradientDrawable drawCircle(int backgroundColor, int borderColor) {
+        // plot rounded button
         GradientDrawable shape = new GradientDrawable();
         shape.setShape(GradientDrawable.OVAL);
         shape.setCornerRadii(new float[]{0, 0, 0, 0, 0, 0, 0, 0});
         shape.setColor(backgroundColor);
+        // stroke width and color
         shape.setStroke(25, borderColor);
         return shape;
+    }
+
+    @Override
+    public void onClick(View v) {
+        // get button id via view
+        int click_btn_id = v.getId();
+        // get info from checklist item
+        int btn_order = mItem.getControl().getButtons().get(click_btn_id).getOrder();
+        int btn_status = mItem.getControl().getButtons().get(click_btn_id).getStatus();
+        Long end = System.currentTimeMillis(); // end time for response
+        mlistener.Completed(Control_TYPE.button,btn_order,btn_status,
+                start, end);
     }
 }
