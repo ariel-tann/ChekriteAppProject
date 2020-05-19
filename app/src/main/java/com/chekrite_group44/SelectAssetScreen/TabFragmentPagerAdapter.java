3da9fd7244b8062c4ba816a7770910e8a2f424f7
Ariel <arieltan.qut@gmail.com>
2020-05-19 19:19:05 +1000

    Some changes

--- app/src/main/java/com/chekrite_group44/SelectAssetScreen/TabFragmentPagerAdapter.java
+++ app/src/main/java/com/chekrite_group44/SelectAssetScreen/TabFragmentPagerAdapter.java
@@ -8,20 +8,25 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
-public class TabFragmentPagerAdapter extends FragmentPagerAdapter{
+import com.chekrite_group44.Keyboard.KeyboardFragment;
+
+public class TabFragmentPagerAdapter extends FragmentPagerAdapter implements SearchAssetFragment.searchAssetListener, KeyboardFragment.keyboardFragmentListener{
     int PAGE_COUNT=3;
     String tabTitles[]=new String[]{"Search","See","Select"};
     Context context;
+    private SearchAssetFragment SearchAssetFragment;
     public TabFragmentPagerAdapter(@NonNull FragmentManager fm, Context context ) {
         super(fm);
         this.context=context;
+
+        SearchAssetFragment = new SearchAssetFragment();
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
         if(position==0)
-            return new SelectTab();
+            return new SearchAssetFragment();
         else if (position==1)
             return new SelectTab();
         else if (position==2)
@@ -38,5 +43,15 @@ public class TabFragmentPagerAdapter extends FragmentPagerAdapter{
     @Override
     public CharSequence getPageTitle(int position) {
         return tabTitles[position];
+    }
+
+    @Override
+    public void onInputKeyboardSent(CharSequence input) {
+        SearchAssetFragment.updateEditText(input);
+    }
+
+    @Override
+    public void onInputSearchAssetSent(CharSequence input) {
+
     }
 }
